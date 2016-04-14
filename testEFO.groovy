086fbabbbe5d77c00388415325fcde4ecfe64d34
Luke Slater <tinmachin3@gmail.com>
2016-04-14 16:33:17 +0100

    try to import go

--- testEFO.groovy
+++ testEFO.groovy
@@ -16,7 +16,41 @@
 import org.semanticweb.owlapi.io.* 
 import org.semanticweb.owlapi.model.*
 import org.semanticweb.owlapi.apibinding.OWLManager
+import org.semanticweb.owlapi.model.AddImport
+
+
+import org.semanticweb.elk.owlapi.ElkReasonerFactory;
+import org.semanticweb.elk.owlapi.ElkReasonerConfiguration
+import org.semanticweb.elk.reasoner.config.*
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.reasoner.*
+import org.semanticweb.owlapi.reasoner.structural.StructuralReasoner
+import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.io.*;
+import org.semanticweb.owlapi.owllink.*;
+import org.semanticweb.owlapi.util.*;
+import org.semanticweb.owlapi.search.*;
+import org.semanticweb.owlapi.manchestersyntax.renderer.*;
+import org.semanticweb.owlapi.reasoner.structural.*
+
 
 OWLOntologyManager man = OWLManager.createOWLOntologyManager();
 OWLDataFactory fac = man.getOWLDataFactory()
 OWLOntology ont = man.createOntology(IRI.create("EFO.ont"))
+
+OWLImportsDeclaration importDeclaration=man.getOWLDataFactory().getOWLImportsDeclaration(IRI.create("http://aber-owl.net/onts/GO_36.ont"));
+man.applyChange(new AddImport(ont, importDeclaration));
+
+ReasonerConfiguration eConf = ReasonerConfiguration.getConfiguration()
+eConf.setParameter(ReasonerConfiguration.NUM_OF_WORKING_THREADS, "8")
+eConf.setParameter(ReasonerConfiguration.INCREMENTAL_MODE_ALLOWED, "true")
+eConf.setParameter(ReasonerConfiguration.INCREMENTAL_TAXONOMY, "true")
+
+OWLReasonerFactory reasonerFactory = new ElkReasonerFactory();
+
+OWLReasonerConfiguration rConf = new ElkReasonerConfiguration(ElkReasonerConfiguration.getDefaultOwlReasonerConfiguration(new NullReasonerProgressMonitor()), eConf)
+OWLReasoner oReasoner = reasonerFactory.createReasoner(ont, rConf);
+oReasoner.precomputeInferences(InferenceType.CLASS_HIERARCHY);
+
+println oReasoner.getEquivalentClasses(fac.getOWLNothing()).getEntitiesMinusBottom().size()
