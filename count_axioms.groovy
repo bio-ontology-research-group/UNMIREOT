ef5d183ddaf7e3e461472d012b220d810c6d6e00
Luke Slater <aberowl@jagannath.pdn.cam.ac.uk>
2016-11-21 23:07:38 +0000

    implrement timeouts and whatnot

--- count_axioms.groovy
+++ count_axioms.groovy
@@ -33,88 +33,110 @@ import org.semanticweb.owlapi.reasoner.structural.*
 import java.io.PrintWriter
 import com.clarkparsia.owlapi.explanation.BlackBoxExplanation;
 import com.clarkparsia.owlapi.explanation.HSTExplanationGenerator;
+import groovy.transform.TimedInterrupt
+import java.util.concurrent.TimeUnit
 import groovy.json.*
 
-def currentDir = new File(".").getAbsolutePath()
-currentDir = currentDir.subSequence(0, currentDir.length() - 1)
-def eConf = ReasonerConfiguration.getConfiguration()
-eConf.setParameter(ReasonerConfiguration.NUM_OF_WORKING_THREADS, "8")
-eConf.setParameter(ReasonerConfiguration.INCREMENTAL_MODE_ALLOWED, "true")
-eConf.setParameter(ReasonerConfiguration.INCREMENTAL_TAXONOMY, "true")
-def reasonerFactory = new ElkReasonerFactory();
-def rConf = new ElkReasonerConfiguration(ElkReasonerConfiguration.getDefaultOwlReasonerConfiguration(new NullReasonerProgressMonitor()), eConf);
-def aCounts = new JsonSlurper().parseText(new File("acount.json").text)
+class CountAxioms {
+  def aCounts = new JsonSlurper().parseText(new File("acount.json").text)
+  def reasonerFactory = new ElkReasonerFactory();
 
-def mireots = new JsonSlurper().parseText(new File("mireot_ontologies.json").text)
-mireots.each { id, refs ->
-  def res = new JsonSlurper().parseText(new File("results/"+id+".json").text)
+  def CountAxioms() {
+    def currentDir = new File(".").getAbsolutePath()
+    currentDir = currentDir.subSequence(0, currentDir.length() - 1)
+    def eConf = ReasonerConfiguration.getConfiguration()
+    eConf.setParameter(ReasonerConfiguration.NUM_OF_WORKING_THREADS, "8")
+    eConf.setParameter(ReasonerConfiguration.INCREMENTAL_MODE_ALLOWED, "true")
+    eConf.setParameter(ReasonerConfiguration.INCREMENTAL_TAXONOMY, "true")
+    def rConf = new ElkReasonerConfiguration(ElkReasonerConfiguration.getDefaultOwlReasonerConfiguration(new NullReasonerProgressMonitor()), eConf);
 
-  res.each { comb, results ->
-    if(!(results instanceof String) && results['count'] > 0) { // there are some unsats
-      def ontology
-      def manager = OWLManager.createOWLOntologyManager()
-      def config = new OWLOntologyLoaderConfiguration();
-      config.setFollowRedirects(true);
-      config.setMissingImportHandlingStrategy(MissingImportHandlingStrategy.SILENT);
+    def mireots = new JsonSlurper().parseText(new File("mireot_ontologies.json").text)
+    mireots.each { id, refs ->
+      def res = new JsonSlurper().parseText(new File("results/"+id+".json").text)
 
-      def combinationPath = "temp/unmireot_test_" + id + "_and_" + comb + ".ontology"
+      res.each { comb, results ->
+        if(!(results instanceof String) && results['count'] > 0) { // there are some unsats
+          def manager = OWLManager.createOWLOntologyManager()
+          def config = new OWLOntologyLoaderConfiguration();
+          config.setFollowRedirects(true);
+          config.setMissingImportHandlingStrategy(MissingImportHandlingStrategy.SILENT);
 
-      try {
+          def combinationPath = "temp/unmireot_test_" + id + "_and_" + comb + ".ontology"
 
-        if(aCounts[id + '_' + comb]) {
-          println "skipping " + id + " and " + comb
-          return
-        }
-
-        ontology = manager
-          .loadOntologyFromOntologyDocument(new IRIDocumentSource(IRI.create("file://"+currentDir+combinationPath)), config);
-
-        def oReasoner = reasonerFactory.createReasoner(ontology, rConf);
-        oReasoner.precomputeInferences(InferenceType.CLASS_HIERARCHY);
-
-        println "[UNMIREOT] Reasoned " + id + " and " + comb + ", resulting in " + oReasoner.getEquivalentClasses(manager.getOWLDataFactory().getOWLNothing()).getEntitiesMinusBottom().size() + " unsatisfiable classes:"
-
-        aCounts[id + '_' + comb] = [:]
-
-        for(OWLClass cl : ontology.getClassesInSignature(true)) {
-          if(!oReasoner.isSatisfiable(cl)) {
-            def iri = cl.getIRI().toString() 
-            if(aCounts['last_iri'] == iri) {
-              println 'skipping because timeout iri'
-              continue;
+          try {
+            if(aCounts.dead_combos.contains(id + '_' + comb)) {
+              println "skipping " + id + " and " + comb
+              return
             }
-            aCounts[id + '_' + comb][iri] = []
-            aCounts['last_iri'] = iri
 
+            def ontology = manager
+              .loadOntologyFromOntologyDocument(new IRIDocumentSource(IRI.create("file://"+currentDir+combinationPath)), config);
+
+            def oReasoner = reasonerFactory.createReasoner(ontology, rConf);
+            oReasoner.precomputeInferences(InferenceType.CLASS_HIERARCHY);
+
+            println "[UNMIREOT] Reasoned " + id + " and " + comb + ", resulting in " + oReasoner.getEquivalentClasses(manager.getOWLDataFactory().getOWLNothing()).getEntitiesMinusBottom().size() + " unsatisfiable classes:"
+            aCounts[id + '_' + comb] = [:]
+            findExplanations(ontology, oReasoner, id, comb)
+          } catch(org.semanticweb.owlapi.io.UnparsableOntologyException e) {
+            println "[UNMIREOT] Unable to parse ontology with " + comb
+          } catch(org.semanticweb.owlapi.model.UnloadableImportException e) {
+            println "[UNMIREOT] Unable to load ontology (unloadable import) with " + comb
+            e.printStackTrace()
+          } catch(org.semanticweb.owlapi.reasoner.InconsistentOntologyException e) {
+            println "[UNMIREOT] Unable to reason ontology with " + comb
+          } catch(e) {
+            println "skipping " + id + '_' + comb
+            aCounts.dead_combos << id + '_' + comb
             new File('acount.json').text = new JsonBuilder(aCounts).toPrettyString()
-
-            BlackBoxExplanation exp = new BlackBoxExplanation(ontology, reasonerFactory, oReasoner);
-            HSTExplanationGenerator multExplanator = new HSTExplanationGenerator(exp);
-
-            Set<Set<OWLAxiom>> explanations=multExplanator.getExplanations(cl);
-            for(Set<OWLAxiom> explanation : explanations) {
-                System.out.println("------------------");
-                System.out.println("Axioms causing the unsatisfiability: ");
-                for (OWLAxiom causingAxiom : explanation) {
-                  System.out.println(causingAxiom);
-                  aCounts[id + '_' + comb][iri] << causingAxiom.toString()
-                }
-                System.out.println("------------------");
-            }
+            println e.getClass().getSimpleName()
           }
         }
-
-      } catch(org.semanticweb.owlapi.io.UnparsableOntologyException e) {
-        println "[UNMIREOT] Unable to parse ontology with " + comb
-      } catch(org.semanticweb.owlapi.model.UnloadableImportException e) {
-        println "[UNMIREOT] Unable to load ontology (unloadable import) with " + comb
-        e.printStackTrace()
-      } catch(org.semanticweb.owlapi.reasoner.InconsistentOntologyException e) {
-        println "[UNMIREOT] Unable to reason ontology with " + comb
-      } catch(e) {
-        println e.getClass().getSimpleName() 
-        e.printStackTrace()
       }
     }
   }
+
+  @TimedInterrupt(value = 10L, unit = TimeUnit.HOURS)
+  def findExplanations(ontology, oReasoner, id, comb) {
+    for(OWLClass cl : ontology.getClassesInSignature(true)) {
+      if(!oReasoner.isSatisfiable(cl)) {
+        def iri = cl.getIRI().toString() 
+        if(aCounts[id + '_' + comb].containsKey(iri)) {
+          println "skipping iri " + iri
+          continue;
+        }
+        aCounts[id + '_' + comb][iri] = []
+        println 'procing ' + iri
+
+        new File('acount.json').text = new JsonBuilder(aCounts).toPrettyString()
+
+        BlackBoxExplanation exp = new BlackBoxExplanation(ontology, reasonerFactory, oReasoner)
+        HSTExplanationGenerator multExplanator = new HSTExplanationGenerator(exp)
+
+        try {
+          getClassExplanations(multExplanator, cl, iri, id, comb)
+        } catch(e) {
+          println e.getClass().getSimpleName()
+          new File('acount.json').text = new JsonBuilder(aCounts).toPrettyString()
+          e.printStackTrace()
+        }
+      }
+    }
+  }
+
+  @TimedInterrupt(value = 5L, unit = TimeUnit.MINUTES)
+  def getClassExplanations(multExplanator, cl, iri, id, comb) {
+    Set<Set<OWLAxiom>> explanations=multExplanator.getExplanations(cl);
+    for(Set<OWLAxiom> explanation : explanations) {
+        //System.out.println("------------------");
+        //System.out.println("Axioms causing the unsatisfiability: ");
+        for (OWLAxiom causingAxiom : explanation) {
+          //System.out.println(causingAxiom);
+          aCounts[id + '_' + comb][iri] << causingAxiom.toString()
+        }
+        //System.out.println("------------------");
+    }
+  }
 }
+
+new CountAxioms()
