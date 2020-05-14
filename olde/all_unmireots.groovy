fa44182cea41bf167d5b97603fb095abc815eabb
Luke Slater <lksr@uhb.nhs.uk>
2020-05-14 23:18:10 +0100

    remove old files

--- olde/all_unmireots.groovy
+++
@@ -1,7 +0,0 @@
-import groovy.json.*
-
-def mireots = new JsonSlurper().parseText(new File("mireot_ontologies.json").text)
-mireots.each { id, refs ->
-  def command = "groovy unmireot.groovy " + id
-  command.execute()
-}
