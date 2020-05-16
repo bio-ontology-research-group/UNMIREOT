8acf4292cea6a7f67583658955e078a8e515b677
Luke Slater <tinmachin3@gmail.com>
2020-05-16 13:07:03 +0100

    Update format_download.groovy
    make correct re readme

--- obo/tools/format_download.groovy
+++ obo/tools/format_download.groovy
@@ -2,6 +2,6 @@
 
 import org.yaml.snakeyaml.Yaml
 
-new File('../ontologies/get_ontologies.sh').text = new Yaml().load(new File('../ontologies.yml').text).ontologies.collect {
+new File('ontologies/get_ontologies.sh').text = new Yaml().load(new File('ontologies.yml').text).ontologies.collect {
   "wget ${it.ontology_purl}"
 }.join('\n')
